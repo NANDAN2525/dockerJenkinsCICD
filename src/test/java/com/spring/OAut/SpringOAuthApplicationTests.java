170923e2fbfe10f37d6a0a2b366b69bfcfd5ecb1
Nandan55 <Nnakirekanti@hottopic.com>
2023-09-15 15:44:50 +0530

    public

--- src/test/java/com/spring/OAut/SpringOAuthApplicationTests.java
+++ src/test/java/com/spring/OAut/SpringOAuthApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringSecurityApplicationTests {
+public class SpringOAuthApplicationTests {
 	
 	@Autowired
 	SpringOAuthApplication app;
@@ -17,5 +17,8 @@ class SpringSecurityApplicationTests {
 	void contextLoads() {
 		assertEquals(app.sonartest(), "Hi in sonar");
 	}
-
+	@Test
+	void sample() {
+		assertEquals("Hi in sonar", "Hi in sonar");
+	}
 }
