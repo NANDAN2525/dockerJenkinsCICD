2144c9b4f1e5c931f31abc0ca4428e3644c1d3e7
Nandan55 <Nnakirekanti@hottopic.com>
2023-09-19 11:36:29 +0530

    constructor in test added

--- src/test/java/com/spring/oauth/SpringOAuthApplicationTests.java
+++ src/test/java/com/spring/oauth/SpringOAuthApplicationTests.java
@@ -7,14 +7,18 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 public class SpringOAuthApplicationTests {
-	
-	@Test
-	public void sample() {
-		assertEquals("Hi in sonar", "Hi in sonar");
-	}
-	
-	@Test
-	public void sample80() {
-		assertEquals("Hi in sonar", "Hi in sonar");
-	}
+
+    @Test
+    public void sample() {
+        assertEquals("Hi in sonar", "Hi in sonar");
+    }
+
+    @Test
+    public void sample80() {
+        assertEquals("Hi in sonar", "Hi in sonar");
+    }
+    
+    public SpringOAuthApplicationTests() {
+        // Constructor logic (if any)
+    }
 }
