d68e6fcfa9abfd826d659276cd23a98af497f147
Nandan55 <Nnakirekanti@hottopic.com>
2023-09-21 11:14:36 +0530

    removed security related

--- src/main/java/com/spring/oauth/SpringOAuthApplication.java
+++ src/main/java/com/spring/oauth/SpringOAuthApplication.java
@@ -2,13 +2,12 @@ package com.spring.oauth;
 
 import java.security.Principal;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
@@ -18,23 +17,23 @@ public class SpringOAuthApplication {
 	private static final Logger logger = LoggerFactory.getLogger(SpringOAuthApplication.class);
 
 	@GetMapping("/")
-	public String greeetings( Principal p) {
+	public String greeetings( ) {
 		 logger.info("In the greetings");
-		    return "Hi 😀😀😀 Welcome " + p.getName();
+		    return "Hi 😀😀😀 Welcome ";
 		
 	}
 	
 	
 	@GetMapping("/demo")
-	public String gregs( Principal p) {
+	public String gregs() {
 		 logger.info("In the demo");
-		return "Hi in demo " +p.getName();		
+		return "Hi in demo 😀😀";		
 	}
 		
 	@GetMapping("/sonar")
 	public String sonartest( ) {
 		 logger.info("In the sonar");
-		return "Hi in sonar"	;	
+		return "Hi in sonar 😀"	;	
 	}
 	
 	
